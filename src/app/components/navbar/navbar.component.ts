fba0ae6e619b94beef7978c3d57fd20a7e823e05
monzon96 <monzon96@umes.edu.gt>
2025-05-01 21:28:22 -0600

    Mejoras en navbar responsive y menú hamburguesa

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; // ⬅️ Importa esto
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule], // ⬅️ Agrégalo aquí
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuAbierto: boolean = false;
+
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  closeMenu() {
+    this.menuAbierto = false;
+  }
+}
